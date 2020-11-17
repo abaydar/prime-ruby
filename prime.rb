468ff46d740e71f0a29e17005d02e7e4ba5123e7
+ <+>
2020-11-17 15:00:37 -0500

    commit tree 11e3fb5217002763168831cc8782a7c95f267b8c

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+require 'pry'
+
+def prime?(num)
+    range = num - 1
+    return false if num < 2
+    (2..range).each do |x|
+        return false if num % x == 0
+    end
+    true
+end
